077c256c631f226fd5655e9f645fab36409d794f
keyasuda <keyasuda@users.noreply.github.com>
2022-10-18 23:47:25 +0900

    rubyの初期化に失敗した場合を画面表示で判別できるようにする

--- js/app.js
+++ js/app.js
@@ -2,22 +2,30 @@ import { RubyApplication } from 'bormashino'
 import rubyWasm from 'url:../tmp/ruby.wasm'
 
 const main = async () => {
-  const vm = await RubyApplication.initVm(rubyWasm, [
-    'ruby.wasm',
-    '-I/stub',
-    '-I/gem/lib',
-    '-EUTF-8',
-    '-e_=0',
-  ])
+  const target = document.querySelector('#bormashino-application')
+  target.innerHTML = 'loading...'
 
-  vm.printVersion()
-  vm.eval(`require_relative '/src/bootstrap.rb'`)
+  try {
+    const vm = await RubyApplication.initVm(rubyWasm, [
+      'ruby.wasm',
+      '-I/stub',
+      '-I/gem/lib',
+      '-EUTF-8',
+      '-e_=0',
+    ])
 
-  const currentPath = () => location.href.replace(location.origin, '')
-  RubyApplication.request('get', currentPath())
-  RubyApplication.mount()
+    vm.printVersion()
+    vm.eval(`require_relative '/src/bootstrap.rb'`)
 
-  window.bormashino = RubyApplication
+    const currentPath = () => location.href.replace(location.origin, '')
+    RubyApplication.request('get', currentPath())
+    RubyApplication.mount()
+
+    window.bormashino = RubyApplication
+  } catch (e) {
+    target.innerHTML = 'error!'
+    console.log(e)
+  }
 }
 
 main()
